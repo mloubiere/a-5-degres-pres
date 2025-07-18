e7a5fe954ce510317927985ed7b9963069ed2982
mloubiere <mloubiere@5degres.com>
2025-07-18 17:03:36 +0200

    Mise à jour de la couleur primaire vers #005953

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,16 @@ export default {
       colors: {
         // Palette 5 Degrés basée sur leur site
         primary: {
-          50: '#f0f9ff',
-          100: '#e0f2fe',
-          200: '#bae6fd',
-          300: '#7dd3fc',
-          400: '#38bdf8',
-          500: '#0ea5e9', // Bleu principal 5 Degrés
-          600: '#0284c7',
-          700: '#0369a1',
-          800: '#075985',
-          900: '#0c4a6e',
+          50: '#f0fdfa',
+          100: '#ccfbf1',
+          200: '#99f6e4',
+          300: '#5eead4',
+          400: '#2dd4bf',
+          500: '#005953', // Vert principal 5 Degrés
+          600: '#004d47',
+          700: '#00403b',
+          800: '#00332f',
+          900: '#002623',
         },
         secondary: {
           50: '#f8fafc',
