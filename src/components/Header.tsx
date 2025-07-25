4384eae193d8ef2db6e38866de40d186f1746b5a
mloubiere <mloubiere@5degres.com>
2025-07-25 12:37:07 +0200

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center gap-3">
           <img 
-            src="public/images/logo5D_only.png" 
+            src="https://bqcbqzqzepyxzyspelou.supabase.co/storage/v1/object/sign/assets/Logo%20360x402.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV80OTBlYWRhYi05MTM2LTQwMDAtOWI5MS03ODhlOTRkZDUxYjEiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhc3NldHMvTG9nbyAzNjB4NDAyLnBuZyIsImlhdCI6MTc1MzQzOTcyMywiZXhwIjoxNzg0OTc1NzIzfQ.xzYWZcD4mP3DK6vXM0rwg5dWSzBBBPf9-FVCVHv671g" 
             alt="5 Degrés Logo" 
             className="h-12 w-auto"
           />
